c61d87bd8c10693c02d5902ad785288789050e24
[avawrist] <chase.michaeldavid@gmail.com>
2022-09-09 08:44:24 -0700

    Added error debug output, window hints

---
+++ game/renderer/source/renderer.cpp
@@ -0,0 +1,96 @@
+// ======================================================================
+// Title: renderer.cpp
+// Description: The primary source file for an untitled renderer.
+// Last Modified: 9/6/2022 MDC
+// ======================================================================
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <windows.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void
+odGLFWError();
+
+void
+printGLFWError();
+
+int
+main()
+{
+    // Initialize GLFW (Enumerates windows, joysticks, starts the timer)
+    if(glfwInit())
+    {
+	// Get any errors from initialization
+        odGLFWError();
+	
+	// Set Window Hints
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+	glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+	glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+        
+	// Set Context Hints
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	// Create window & context
+	GLFWwindow* window = glfwCreateWindow(640, 480, "First Game", glfwGetPrimaryMonitor(), NULL);
+	odGLFWError();
+
+	if(window)
+	{
+	    while(true)
+	    {
+		
+	    }
+
+	    // Delete window
+	    glfwDestroyWindow(window);
+	}
+	else
+	{
+	    OutputDebugStringA("ERROR: GLFW failed to create window/context\n");
+	}
+	
+	// Terminate GLFW on program exit:
+	glfwTerminate();
+    }
+    else
+    {
+	OutputDebugStringA("ERROR: Failed to initialize GLFW\n");
+    }
+    
+    return 0;
+}
+
+
+void
+odGLFWError()
+{
+    const char* errorMsg;
+    int errorCode = glfwGetError(&errorMsg);
+    if(errorMsg)
+    {
+        OutputDebugStringA(errorMsg);
+	OutputDebugStringA("\n");
+    }
+}
+
+void
+printGLFWError()
+{
+    const char* errorMsg;
+    int errorCode = glfwGetError(&errorMsg);
+    if(errorMsg)
+    {
+	cout << "GLFW Error: " << errorMsg << "ID: " << errorCode << endl;
+    }
+}
