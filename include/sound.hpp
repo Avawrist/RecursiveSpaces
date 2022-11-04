b51765cb3a52beb97e4a9dcf444eff9fae7fa677
[avawrist] <chase.michaeldavid@gmail.com>
2022-11-04 08:58:17 -0700

    Started implementing MS audio api

--- include/sound.hpp
+++ include/sound.hpp
@@ -1,11 +1,36 @@
-// ======================================================================
+// =============================================================================
 // Title: sound.hpp
-// Description: The header file for game sounds
-// ======================================================================
+// Description: The header file for game sounds, built on Microsoft DirectSound
+// =============================================================================
 
 #ifndef SOUND_H
 #define SOUND_H
 
-#include <dsound.h>
+#include <commdlg.h>
+
+// STREAMING WAV FILES
+// Primary buffer must be from the buffer class
+
+// Secondary buffers are from the SecondaryBuffer class
+
+// For short sounds, DirectSound can load static WAV files into a buffer for us.
+// For streaming sounds (i.e. full songs), we must parse the WAV file and copy the data
+// into the buffer ourselves. 
+
+// DEVICES
+
+////////////////////////
+// SoundDevice Struct //
+////////////////////////
+
+typedef struct SoundDevice {
+    HRESULT ds_device;
+    SoundDevice();
+    ~SoundDevice();
+} SoundDevice;
 
 #endif
+
+// openAL-soft
+// Wwise
+// FMOD
--- src/sound.cpp
+++ src/sound.cpp
@@ -4,3 +4,20 @@
 // ======================================================================
 
 #include "sound.hpp"
+
+////////////////////////
+// SoundDevice Struct //
+////////////////////////
+
+SoundDevice::SoundDevice()
+{
+    // Get device
+    LPDIRECTSOUND8 lpds;
+    ds_device = DirectSoundCreate8(NULL, &lpds, NULL);
+    // SetCooperativeLevel
+}
+
+SoundDevice::~SoundDevice()
+{
+
+}
